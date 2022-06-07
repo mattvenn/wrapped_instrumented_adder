18c41af3204a75badae66880eda207f8df374865
Matt Venn <matt@mattvenn.net>
2022-06-07 14:25:05 +0200

    worked

--- config.tcl
+++ config.tcl
@@ -16,7 +16,7 @@ set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/wrapper.v \
 set ::env(SYNTH_READ_BLACKBOX_LIB) 1
 
 # target density, change this if you can't get your design to fit
-set ::env(PL_TARGET_DENSITY) 0.1
+set ::env(PL_TARGET_DENSITY) 0.20
 #set ::env(GLB_RT_ALLOW_CONGESTION) 1
 #set ::env(FP_CORE_UTIL) 35
 #set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
@@ -26,7 +26,7 @@ set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
 
 # because the area we need is constrained by the number of pins, replace kept failing
 # set absolute size of the die to 300 x 300 um
-set ::env(DIE_AREA) "0 0 350 350"
+set ::env(DIE_AREA) "0 0 370 370"
 set ::env(FP_SIZING) absolute
 
 # define number of IO pads
